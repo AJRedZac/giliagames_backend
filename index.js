bc9dd8ab723a068e6e029466a7ccc12af3f938b1
AJRedZac <ponchovi94@gmail.com>
2025-06-29 21:45:47 -0600

    removing deprecated options for mongodb connection

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('✅ Conectado a MongoDB'))
 .catch((err) => console.error('❌ Error al conectar a MongoDB:', err));
 const Visit = require('./models/visit');
